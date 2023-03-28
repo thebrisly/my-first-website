780d5d387a537a74d16adbd58c168a09882aea1e
Lau <laura.fabbiano.gomez@hotmail.com>
2023-03-28 22:24:23 +0200

    learned how to implement js and multiple things on CSS

---
+++ javascript/travel.js
@@ -0,0 +1,8 @@
+/*let visitedCount = 0;
+const visitedCountElem = document.getElementById("visited-count");
+
+function markVisited(area) {
+  area.style.fill = "#abcdef";
+  visitedCount++;
+  visitedCountElem.innerText = visitedCount;
+}*/
